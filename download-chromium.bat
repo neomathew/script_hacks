42df4a3a5606c7edc1398a2f246f24b39929d8e6
sun-wolf <35052183+neomathew@users.noreply.github.com>
2022-05-12 23:06:00 +1000

    Update download-chromium.bat

--- download-chromium.bat
+++ download-chromium.bat
@@ -1,5 +1,7 @@
 for /F %%I in ('curl https://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/LAST_CHANGE') do set LATEST=%%I
 echo %LATEST%
 curl --silent -L -O "http://commondatastorage.googleapis.com/chromium-browser-snapshots/Win/%LATEST%/chrome-win.zip"
+ping 127.0.0.1
 copy "chrome-win.zip" "%CD%\chrome-win-copy.zip" /Y
+ping 127.0.0.1
 tar -cvzf "chrome-win-copy.zip" "%CD%\chrome-win"
